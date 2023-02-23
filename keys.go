fc33f194226b9c34771dc833faf04138c2b266b2
sgcatalyzed <102430128+sgcatalyzed@users.noreply.github.com>
2023-02-22 18:14:35 -0800

    Interquery Types

---
+++ keys.go
@@ -0,0 +1,26 @@
+package types
+
+const (
+	// ModuleName defines the module name
+	ModuleName = "interchainquery"
+
+	// StoreKey defines the primary module store key
+	StoreKey = ModuleName
+
+	// RouterKey is the message route for slashing
+	RouterKey = ModuleName
+
+	// QuerierRoute defines the module's query routing key
+	QuerierRoute = ModuleName
+)
+
+// prefix bytes for the interchainquery persistent store
+const (
+	prefixData  = iota + 1
+	prefixQuery = iota + 1
+)
+
+var (
+	KeyPrefixData  = []byte{prefixData}
+	KeyPrefixQuery = []byte{prefixQuery}
+)
